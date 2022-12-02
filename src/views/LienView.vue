7ac1b6364a890249af00daa2a555d80bdefead78
Moh2106 <msalifou149@gmail.com>
2022-12-02 17:32:59 +0100

    Réajustement de la page Liens

--- src/views/LienView.vue
+++ src/views/LienView.vue
@@ -1,9 +1,44 @@
 <template>
   <div class="pt-14 liens">
 
-    <div class="flex justify-center m-4">
+    <div class="flex justify-center mt-4">
         <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-            <img class="w-full h-96 object-cover rounded-t-lg md:rounded-none md:rounded-l-lg liens__image" src="../assets/Liens/logo_gite_france.png" alt="Logo de gite de France">
+            <img class="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="../assets/Liens/logo_gite_france.png" alt="" />
+            <div class="p-6 flex flex-col justify-start">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Gite le cormier sur le site de Gite de France</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    Vous pouvez faire des réservations via ce site.
+                </p>
+
+                <button>
+                    <a href="https://www.gites-de-france.com/fr/normandie/eure/gite-du-cormier-27g818" target="_blank" rel="noopener noreferrer">Allez sur le site de réservation</a>
+                </button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="flex justify-center m-4">
+            <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                <img class="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="../assets/Liens/logo_gite_france.png" alt="" />
+
+            <div class="p-6 flex flex-col justify-start">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Gite le cormier sur le site de Gite de France Eure</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    Vous pouvez faire des réservations via ce site.
+                </p>
+
+                <button>
+                    <a href="https://www.gites-de-france-eure.com/location-vacances-Gite-Hauville-27G818.html" target="_blank" rel="noopener noreferrer">Allez sur le site de réservation</a>
+                </button>
+
+            </div>
+        </div>
+    </div>
+
+    <!----<div class="flex justify-center m-4">
+        <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+            <img class="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg liens__image" src="../assets/Liens/logo_gite_france.png" alt="Logo de gite de France">
             <div class="p-6 flex flex-col justify-start">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">Gite le cormier sur le site de Gite de France</h5>
                 <p class="text-gray-700 text-base mb-4">
@@ -15,9 +50,9 @@
                 </button>
             </div>
         </div>
-    </div>
+    </div>-->
 
-    <div class="flex justify-center m-4">
+    <!----<div class="flex justify-center m-4">
         <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
 
             <img class="w-full h-96 object-cover rounded-t-lg md:rounded-none md:rounded-l-lg liens__image" src="../assets/Liens/logo_gite_france.png" alt="Logo de gite de France">
@@ -33,7 +68,7 @@
                 </button>
             </div>
         </div>
-    </div>
+    </div>-->
 
   </div>
 </template>
@@ -51,6 +86,8 @@ export default {
         }
 
         &__image { 
+            width: 100%;
+
             @media screen and (max-width:768px) {
                 height: 15em;
                 width: 90%;
