b86541e2723f69e88dd7aba41d2d2cb88488e002
Moh2106 <msalifou149@gmail.com>
2023-02-11 14:28:09 +0100

    Mis à jour de la page reservation 2

--- src/views/ReservationView.vue
+++ src/views/ReservationView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="pt-16 reservation">
-    <!--<div class="w-1/3 m-auto reservation__iframe">
+    <div class="w-1/2 m-auto reservation__iframe">
       <iframe 
         style="overflow:auto; -webkit-overflow-scrolling:touch;border:none;" 
         width="100%"
-        height="600"
-        src="widget.itea.fr/widget.php?key=vk3_8_ia&numGite=818&widget-itea=resa&nbmois=6&nbmoisLigne=3">
+        height="1200"
+        src="https://widget.itea.fr/widget.php?key=vk3_8_ia&numGite=818&widget-itea=resa&nbmois=3&avecPrix=1&nbmoisLigne=3">
       </iframe>
-    </div>-->
 
-    <div data-widget-itea="resa" data-key="vk3_8_ia" data-numgite="818" data-nbmois="3" data-nbmois-ligne="3" data-avecPrix="1"></div>
+
+    </div>
+
+    <!--<div data-widget-itea="resa" data-key="vk3_8_ia" data-numgite="818" data-nbmois="3" data-nbmois-ligne="3" data-avecPrix="1"></div>-->
 
     <!--<div data-widget-itea="prix" data-key="vk3_8_ia" data-numgite="818"></div>
 
